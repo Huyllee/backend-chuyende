fcbc310c949f2f0d5cfe48e85043d0ab5d7f91de
Huyllee <moriarty1807@gmail.com>
2023-05-23 22:35:19 +0700

    update database

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,11 @@
-// import express from "express";
-// import cors from "cors";
-// import mysql from 'mysql';
-// import bodyParser from 'body-parser';
-// import foodRouter from './routers/food.router'
-// import userRouter from './routers/user.router'
-
-// const app = express();
-
-// app.use(express.json());
-
-// app.use(cors({
-//     credentials: true,
-//     origin:["http://localhost:4200"]
-// }))
-
-// app.use("/api/foods", foodRouter);
-// app.use("/api/users/", userRouter);
-        
-// const port = 5000;
-// app.listen(port, () => {
-//     console.log("Website server on http://localhost:" + port)
-// })
-
-
-
-
-
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const mysql = require('mysql');
-
 import express from "express";
 import cors from "cors";
 import mysql from 'mysql';
 import bodyParser from 'body-parser';
 import userRouter from './routers/user.router'
 import novelRouter from "./routers/novel.router";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const server = express();
 
@@ -47,27 +16,42 @@ server.use(cors({
     origin:["http://localhost:4200"]
 }))
 
-const port = 5000;
-
-export const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'admin',
-    password: '123456',
-    database: 'dbtt'
-})
-
-db.connect(function(error: mysql.MysqlError) {
-    if(error){
-        console.log('Error Connecting to DB')
-    }else{
-        console.log('Succsessfully Connected to DB')
-    }
-});
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
 
 
-server.listen(port, () => {
-    console.log("Website server on http://localhost:" + port)
-});
+// const dbUser = process.env.DB_USER;
+// const dbPassword = process.env.DB_PASSWORD;
+// const dbDatabase = process.env.DB_DATABASE;
+
+// const dbUser = process.env.DB_USER;
+// const dbPassword = process.env.DB_PASSWORD;
+// const dbHost = process.env.DB_HOST;
+// const dbPort = process.env.DB_PORT;
+// const dbName = process.env.DB_NAME;
+
+// const port = dbPort;
+
+// export const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: dbUser,
+//     password: dbPassword,
+//     database: dbDatabase
+// })
+
+export const db = mysql.createConnection(urlDB)
+
+// db.connect(function(error: mysql.MysqlError) {
+//     if(error){
+//         console.log('Error Connecting to DB')
+//     }else{
+//         console.log('Succsessfully Connected to DB')
+//     }
+// });
+
+
+// server.listen(port, () => {
+//     console.log("Website server on http://localhost:" + port)
+// });
 
 
 
